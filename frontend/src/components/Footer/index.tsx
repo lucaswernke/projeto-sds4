d53dc8992e429b10eb4bc5cd1a63bf9916de51dc
Lucas Wernke <englucaswernke@hotmail.com>
2021-09-06 22:18:57 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/lucaswernke" target="_blank" rel="noreferrer">Lucas Wernke</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/lucaswernke" target="_blank" rel="noreferrer">@lucaswernke</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
