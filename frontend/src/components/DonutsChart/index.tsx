ba58072f1089e29d8dd5f984d97972d83ed9c6cf
Lucas Wernke <englucaswernke@hotmail.com>
2021-09-11 10:53:31 -0300

    DonutChart integration

--- frontend/src/components/DonutsChart/index.tsx
+++ frontend/src/components/DonutsChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,36 +11,37 @@ type ChartData = {
 
 const DonutsChart = () => {
 
-    let chartData : ChartData = { labels: [], series: []};
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then(response => {
-        const data = response.data as SaleSum[];
-        const myLabels = data.map(x => x.sellerName);
-        const mySeries = data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-        chartData = { labels: myLabels, series: mySeries};
-        console.log(chartData);
-    });
-         
-        //const mockData = {
-     //   series: [477138, 499928, 444867, 220426, 473088],
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
+
+    //const mockData = {
+    //   series: [477138, 499928, 444867, 220426, 473088],
     //  labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']1
     //}
-    
+
     const options = {
         legend: {
             show: true
         }
     }
 
-    return (    
+    return (
         <Chart
-            options={{ ...options, labels: chartData.labels}}     
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
-            height="240"              
-        />   
+            height="240"
+        />
     );
 }
 
